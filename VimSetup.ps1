05c59d0f1bc4e0b901c4164e5abc8f808ff89264
devshorts <akropp@gmail.com>
2014-03-08 12:49:02 -0800

    quick commit

--- VimSetup.ps1
+++ VimSetup.ps1
@@ -4,13 +4,13 @@ Set-Alias vi   $VIMPATH
 Set-Alias vim  $VIMPATH
 
 # for editing your PowerShell profile
-Function Edit-Profile
+function Edit-Profile
 {
     vim $profile
 }
 
 # for editing your Vim settings
-Function Edit-Vimrc
+function Edit-Vimrc
 {
     vim $home\_vimrc
 }
